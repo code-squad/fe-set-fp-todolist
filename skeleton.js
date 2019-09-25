a4c7b22c251ff665d50f612bbe4ba39cde883209
zoroaster23 <44594105+zoroaster23@users.noreply.github.com>
2019-09-25 19:48:49 +0900

    skeleton 코드 추가

---
+++ skeleton.js
@@ -0,0 +1,42 @@
+let todoList = {};
+const formatSet = {};
+
+// 명령과 입력값으로 나눈다.
+const parseInput = () => {
+    
+}
+
+//입력을 받고 명령어에 알맞은 함수를 호출한다.
+const getInput = () => {
+
+}
+
+const doCommand = (command, args) => {
+
+}
+
+//입렵된 todolist를 반환한다.
+const getTodoList = () => {
+    return getTodoList;
+}
+
+// todoItem을 추가한다.
+const addTodoItem = () => {
+
+}
+
+// todoList의 상태를 변환한다. 
+const changeStatus = (uid, status) => {
+    return todoItem;
+}
+
+//정해진 형식에 따라 출력 문자열을 만든다.
+const formatter = (args, formatFn) => {
+
+}
+
+const init = () => {
+
+}
+
+
